b13a492291b25aaa2f57b5efdb8204c2bea9c1ee
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2023-05-05 15:21:21 +0300

    Add files via upload

---
+++ 2 procedural programming task.c
@@ -0,0 +1,150 @@
+/*
+This program takes an integer value N as input, followed by N integers.
+ It then splits the array of integers into two subarrays,
+ one containing all the negative integers and the other containing all the non-negative integers. 
+ After sorting both subarrays,
+ the program calculates the sum of the product of the corresponding elements in each subarray and prints the result.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print(int *S)
+{
+	printf("Result is %d", *S);   
+}
+
+void count(int N, int a[], int* S, int xCount, int yCount)
+{
+    int x[xCount];
+    int y[yCount];   
+    int swapTemp = 0;
+    int p = 0;             
+     
+    for (int i = 0; i < N-1; ++i)
+    {
+        for (int j = i+1; j < N; ++j)
+        {
+            if (a[i] > a[j])
+            { 
+                swapTemp = a[i];
+                a[i] = a[j];
+                a[j] = swapTemp;
+            }
+        }
+    } 
+
+	for(int i = 0; i < xCount; ++i)
+	{
+		x[i] = a[i];
+		
+    }
+			
+	for(int i = xCount; i < N; ++i)
+	{
+		y[i] = a[i];	
+	}
+    if (xCount < yCount)
+    {
+        p = xCount;   
+    }
+    else
+        p = yCount;
+	   
+    for(int i = 0; i < p; ++i)
+    {
+		printf("%d\n",y[i]);
+        *S += (x[i]*y[i]);					
+    }
+}
+
+void scan_elements(int N, int a[], int *xCount, int *yCount)
+{
+    char c;
+    int temp;
+    int input;
+    *xCount = 0;
+    *yCount = 0;
+
+    printf("Enter %d integers.\n", N);
+
+    for(int i = 0; i < N; ++i)
+    {
+        temp = scanf("%d", &input);
+
+        if(temp == 1 || getchar() != '\n')
+        {
+            a[i] = input;
+        }
+        
+        else
+        {
+           printf("Value you have entered is invalid, try again.\n");
+        }
+    } 
+    
+    for (int i = 0; i < N; ++i)
+    {
+        if(a[i] < 0)
+        {
+            ++*xCount;
+        }
+        else
+        {
+            ++*yCount;
+        }
+        
+    }
+    
+    if(*xCount == N)
+    {
+        printf("All members of array are negative.\n");
+        exit(0);
+    }
+    
+    if(*yCount == N)
+    {
+        printf("All members of array are not negative.\n");
+        exit(0);
+    }
+    
+    
+
+}
+ 
+void scan_size(int* N)
+{   
+    int temp;
+    char c;
+
+    printf("Enter the size of array (an integer positive value).\n");
+
+    while(*N <= 0)
+    {   
+        temp = scanf("%d", N);
+        
+        // checks if N is valid
+        if (temp == 1 && *N > 0 || getchar() != '\n')
+        {
+        }
+        else
+        {
+            printf("Value you have entered is invalid, try again.\n");
+        }    
+    }
+    printf("Size of array scanned succesfully.\n");
+}
+
+int main()
+{
+    int N = 0;
+    int a[N];
+    int S = 0;
+	int xCount = 0;
+	int yCount = 0;
+    scan_size(&N);
+    scan_elements(N, a, &xCount, &yCount);
+    count(N, a, &S, xCount, yCount);
+    print(&S);
+    return 0;
+}
