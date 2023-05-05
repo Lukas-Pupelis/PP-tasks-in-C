f35dd0f08673d77cc40270b583b3782f42cfd789
Lukas Pupelis <105020330+Lukas-Pupelis@users.noreply.github.com>
2023-05-05 15:34:28 +0300

    Update 2 procedural programming task.c
    dynamic memory allocation instead of static arrays.

--- 2 procedural programming task.c
+++ 2 procedural programming task.c
@@ -1,60 +1,52 @@
-/*
-This program takes an integer value N as input, followed by N integers.
- It then splits the array of integers into two subarrays,
- one containing all the negative integers and the other containing all the non-negative integers. 
- After sorting both subarrays,
- the program calculates the sum of the product of the corresponding elements in each subarray and prints the result.
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void print(int *S)
 {
-	printf("Result is %d", *S);   
+    printf("Result is %d", *S);
 }
 
 void count(int N, int a[], int* S, int xCount, int yCount)
 {
     int x[xCount];
-    int y[yCount];   
+    int y[yCount];
     int swapTemp = 0;
-    int p = 0;             
-     
+    int p = 0;
+
     for (int i = 0; i < N-1; ++i)
     {
         for (int j = i+1; j < N; ++j)
         {
             if (a[i] > a[j])
-            { 
+            {
                 swapTemp = a[i];
                 a[i] = a[j];
                 a[j] = swapTemp;
             }
         }
-    } 
-
-	for(int i = 0; i < xCount; ++i)
-	{
-		x[i] = a[i];
-		
     }
-			
-	for(int i = xCount; i < N; ++i)
-	{
-		y[i] = a[i];	
-	}
+
+    for(int i = 0; i < xCount; ++i)
+    {
+        x[i] = a[i];
+    }
+
+    for(int i = xCount; i < N; ++i)
+    {
+        y[i-xCount] = a[i];
+    }
+
     if (xCount < yCount)
     {
-        p = xCount;   
+        p = xCount;
     }
     else
         p = yCount;
-	   
+
     for(int i = 0; i < p; ++i)
     {
-		printf("%d\n",y[i]);
-        *S += (x[i]*y[i]);					
+        *S += (x[i]*y[i]);
     }
 }
 
@@ -76,13 +68,14 @@ void scan_elements(int N, int a[], int *xCount, int *yCount)
         {
             a[i] = input;
         }
-        
+
         else
         {
-           printf("Value you have entered is invalid, try again.\n");
+            printf("Value you have entered is invalid, try again.\n");
+            i--;
         }
-    } 
-    
+    }
+
     for (int i = 0; i < N; ++i)
     {
         if(a[i] < 0)
@@ -93,25 +86,22 @@ void scan_elements(int N, int a[], int *xCount, int *yCount)
         {
             ++*yCount;
         }
-        
+
     }
-    
+
     if(*xCount == N)
     {
         printf("All members of array are negative.\n");
         exit(0);
     }
-    
+
     if(*yCount == N)
     {
         printf("All members of array are not negative.\n");
         exit(0);
     }
-    
-    
-
 }
- 
+
 void scan_size(int* N)
 {   
     int temp;
@@ -126,6 +116,8 @@ void scan_size(int* N)
         // checks if N is valid
         if (temp == 1 && *N > 0 || getchar() != '\n')
         {
+            // Consume any extra input characters, if any
+            while ((c = getchar()) != '\n' && c != EOF);
         }
         else
         {
@@ -138,13 +130,26 @@ void scan_size(int* N)
 int main()
 {
     int N = 0;
-    int a[N];
+    int *a;
     int S = 0;
-	int xCount = 0;
-	int yCount = 0;
+    int xCount = 0;
+    int yCount = 0;
+
     scan_size(&N);
+
+    // Dynamically allocate memory for array 'a'
+    a = (int*) malloc(N * sizeof(int));
+    if (a == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
     scan_elements(N, a, &xCount, &yCount);
     count(N, a, &S, xCount, yCount);
     print(&S);
+
+    // Free dynamically allocated memory
+    free(a);
+
     return 0;
 }
